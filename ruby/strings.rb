e318fcb984ffef8d5a0b0f3b30df01a7d896ad98
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-07-06 20:23:16 -0300

    Add files via upload

---
+++ ruby/strings.rb
@@ -0,0 +1,15 @@
+puts "Daniel".downcase
+
+puts "Daniel".upcase
+
+puts "          muito espaço em branco     ".strip
+
+nome = "Bruno Alex"
+nome["Alex"] = "Daniel"
+puts nome
+
+puts "jeep renegade".capitalize
+
+puts "tesla".chars
+
+puts "Jeep Renegade".split
