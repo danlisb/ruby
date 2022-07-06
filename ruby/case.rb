b8590c9b417be28ecff5ba5354be20a0c8ec09ec
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-07-06 16:13:48 -0300

    Add files via upload

---
+++ ruby/case.rb
@@ -0,0 +1,21 @@
+marca = "tesla"
+
+case marca
+when "tesla"
+  puts "TESLA"
+when "ford"
+  puts "FORD"
+when "fiat"
+  puts "FIAT"
+else
+  puts "SEM MARCA"
+end
+
+case
+when 2 < 1
+  puts "false"
+when 5 > 3
+  puts "true"
+else
+  "não existe tal condição"
+end
