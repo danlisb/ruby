ade76b8602fe7059bc56e959a7f7d7f4ce7d8669
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-07-06 20:30:17 -0300

    Add files via upload

---
+++ ruby/symbols.rb
@@ -0,0 +1,7 @@
+#symbol é utilizado para economizar memória no programa por não precisar redeclarar variáveis
+
+:nome_completo #padrão de declaração de symbols
+
+puts "nome".to_sym #transformar string em symbol
+
+puts :nome.to_s #transformar symbol em string
