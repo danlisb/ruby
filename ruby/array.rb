23d90ba99633aca71a5dba928da764009cf96462
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-07-06 21:04:41 -0300

    Add files via upload

---
+++ ruby/array.rb
@@ -0,0 +1,19 @@
+print lista = ['nome', :nome, 1, 2, 2.5, 4, 3]
+
+print lista_dois = %w[conversão de strings para array]
+
+puts lista_dois.size
+
+puts lista.empty?
+
+puts lista_dois[0]
+puts lista_dois[1]
+puts lista_dois[-1]
+puts lista_dois[-2]
+
+lista_dois[0] = "daniel"
+print lista_dois
+
+print lista_dois.push 10
+
+print lista_dois.join '.'
