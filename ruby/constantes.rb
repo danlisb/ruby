20abe66abee79eaf52fc0937b4386d3934a3de0f
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-07-11 03:48:07 -0300

    Add files via upload

---
+++ ruby/constantes.rb
@@ -0,0 +1,11 @@
+#constantes são declaradas com letras maiúsculas
+#podem ser alteradas, porém, retorna-se uma mensagem de erro
+
+puts MAX_USUARIOS = 10
+puts MAX_USUARIOS = 100
+
+class Carro
+  MODELOS_DISPONIVEIS = ["Model X", "Model S"]
+end
+
+print Carro::MODELOS_DISPONIVEIS
